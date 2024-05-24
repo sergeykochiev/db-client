5e77362900ad67eb78f028b346b6f2ba9564412d
sergeykochiev <sergeyivanovo547@gmail.com>
2024-05-24 17:45:45 +0300

    13

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,8 +4,8 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     stretched?: boolean
 }
 
-export default function Button({ stretched = false, ...props}) {
+export default function Button({ stretched = false, ...props}: ButtonProps) {
     return (
-    <button className={`${stretched ? "w-full px-4 p-2" : "max-w-max aspect-square p-3"} transition-all grid place-items-center justify-self-end rounded-full font-bold outline outline-blue-400 outline-2 text-blue-400 -outline-offset-2 bg-slate-200 hover:bg-blue-400 hover:text-slate-100`} {...props}>{props.children}</button>
+        <button className={`${stretched ? "w-full px-4 p-2" : "max-w-max aspect-square p-3"} transition-all grid place-items-center justify-self-end rounded-full font-bold outline outline-blue-400 outline-2 text-blue-400 -outline-offset-2 bg-slate-200 hover:bg-blue-400 hover:text-slate-100`} {...props}>{props.children}</button>
     )
 }
